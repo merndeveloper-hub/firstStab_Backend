741645bd6e7bf7c0d9afe0b67f7430c4d355e1ae
Owais Yyameen <owais.yameen@hbl.com>
2025-07-21 19:24:28 +0500

    changes in newrRequest pro

--- routes/user/serviceDetail/firestore/socketHandlerSender.js
+++ routes/user/serviceDetail/firestore/socketHandlerSender.js
@@ -30,37 +30,37 @@ const handleSocket = (io) => {
         proBooking,
         isBooking,
       }) => {
-        try{
-        if (isBooking) {
-          const findUserChat = await find("chatMessage", {
-            role: "user",
-            chatId,
-            userBooking,
-            proBooking,
-          });
-          if (findUserChat.length <= 7) {
-            socket.emit("chat_error", {
-        status: "error",
-        message: "User chat limit exceeded",
-      });
-      return;
-          }
-          const findProChat = await find("chatMessage", {
-            role: "pro",
-            chatId,
-            userBooking,
-            proBooking,
-          });
-          if (findProChat.length <= 7) {
-            socket.emit("chat_error", {
-        status: "error",
-        message: "Pro chat limit exceeded",
-      });
-      return;
+      //   try{
+      //   if (isBooking) {
+      //     const findUserChat = await find("chatMessage", {
+      //       role: "user",
+      //       chatId,
+      //       userBooking,
+      //       proBooking,
+      //     });
+      //     if (findUserChat.length <= 7) {
+      //       socket.emit("chat_error", {
+      //   status: "error",
+      //   message: "User chat limit exceeded",
+      // });
+      // return;
+      //     }
+      //     const findProChat = await find("chatMessage", {
+      //       role: "pro",
+      //       chatId,
+      //       userBooking,
+      //       proBooking,
+      //     });
+      //     if (findProChat.length <= 7) {
+      //       socket.emit("chat_error", {
+      //   status: "error",
+      //   message: "Pro chat limit exceeded",
+      // });
+      // return;
     
-          }
+      //     }
         
-        }
+      //   }
       
         //       const findChatRoom = await findOne("proBookingService",{chatChannelName:chatId})
         //      let startTime = findChatRoom.orderStartTime
@@ -79,27 +79,29 @@ const handleSocket = (io) => {
         });
         console.log(newMessage, "messagenew");
 
-        socket.emit("message_sent", newMessage);
-      }catch (err) {
-  socket.emit("chat_error", {
-    status: "error",
-    message: "An unexpected error occurred",
-    error: err.message,
-  });
+        socket.to(senderId).emit("message_sent", newMessage);
+         socket.to(receiverId).emit("receive_message", newMessage);
+      //}
+  //     catch (err) {
+  // socket.emit("chat_error", {
+  //   status: "error",
+  //   message: "An unexpected error occurred",
+  //   error: err.message,
+  // });
 }
-    }
+   // }
     );
     console.log("final");
 
-    // socket.on('disconnect', () => {
-    //   for (let [userId, socketId] of onlineUsers.entries()) {
-    //     if (socketId === socket.id) {
-    //       onlineUsers.delete(userId);
-    //       break;
-    //     }
-    //   }
-    //   console.log('[Socket] Disconnected:', socket.id);
-    // });
+    socket.on('disconnect', () => {
+      for (let [userId, socketId] of onlineUsers.entries()) {
+        if (socketId === socket.id) {
+          onlineUsers.delete(userId);
+          break;
+        }
+      }
+      console.log('[Socket] Disconnected:', socket.id);
+    });
   });
 };
 
